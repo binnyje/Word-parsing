2ede3928caed9a82ea1dbbc3196c0218a39c0ddb
binnyje <122936599+binnyje@users.noreply.github.com>
2023-02-26 22:36:09 -0500

    Initialsetup

---
+++ src/main/java/com/sample/WordParsing/WordParsingApplication.java
@@ -0,0 +1,13 @@
+package com.sample.WordParsing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordParsingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordParsingApplication.class, args);
+	}
+
+}
